98e2de46951c1f5c310b4190b3cfa262e66e45b4
Kushwanth P <Kushwanth.P@Cognizant.com>
2023-11-04 00:37:09 +0530

    create user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,34 @@
+import { errorHandler } from "../utils/error.js"
+import bcrypt from "bcryptjs";
+import User from '../models/user.model.js'
+
 export const test = (req, res) => {
     res.json({
         message: 'Hello Jake'
     })
+}
+
+export const updateUser = async (req, res, next) => {
+    console.log("KP req.params", req.params)
+    console.log("KP req.user", req.user)
+    if (req.user.id !== req.params.id) return next(errorHandler(401, "You are unauthorized"))
+    try {
+        if (req.body.password) {
+            req.body.password = bcrypt.hashSync(req.body.password, 10)
+        }
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar
+            }
+        }, { new: true })
+
+        const { password, ...rest } = updatedUser._doc
+
+        res.status(200).json(rest)
+    } catch (error) {
+        next(error)
+    }
 }
