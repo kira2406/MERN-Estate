f97c3cf9e3a45dd35eddfd80a0d29a858a833366
Kushwanth P <Kushwanth.P@Cognizant.com>
2023-11-03 18:15:51 +0530

    Completed signup page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Enter Username"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="Enter Email"
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+        <button className="bg-green-500 text-white rounded-lg uppercase p-3 hover:opacity-95 disabled:opacity-80">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link>
+          <span className="text-blue-700">Sign In</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
