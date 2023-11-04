21c534301bea04b19b2ef250bfc291eef39d4869
Kushwanth P <Kushwanth.P@Cognizant.com>
2023-11-04 22:53:08 +0530

    create search page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,10 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
-import Listing from "./pages/Listing";
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
